228028a083cc3d91b93bf8c58233ac8305c79542
Adair <quesuabe@gmail.com>
2025-01-17 23:17:44 -0600

    CambiosCss

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,18 @@
-import "./home.css";
+import styles from "./home.module.css";
 
 function Home() {
   return (
-    <div>
-      <p>Hola Mundo</p>
+    <div className={styles.contenedor}>
+      <div className={styles.he}>
+        <h1>Adair Maldonado</h1>
+        <h2>
+          <span>FrontEnd</span> Developer<span>.</span>
+        </h2>
+        <button className="btn">Leer más →</button>
+        <ul>
+          <li>hola</li>
+        </ul>
+      </div>
     </div>
   );
 }
