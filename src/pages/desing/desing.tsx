e93555c0aa903736f734315d09e817df419e3bb6
Adair <quesuabe@gmail.com>
2025-01-26 21:05:36 -0600

    cambios Provisionales

--- src/pages/desing/desing.tsx
+++ src/pages/desing/desing.tsx
@@ -1,9 +1,44 @@
-import "./desing.css";
+import styles from "./desing.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+import { EffectCoverflow, Pagination } from "swiper/modules";
 
 function Desing() {
   return (
     <div>
-      <h1>Diseño Grafico / Diseño Web </h1>
+      <h1 className={styles.titulo}>Trabajos de Diseñador</h1>
+      <div className={styles.container}>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={"auto"}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination]}
+          className={styles.swiper}
+        >
+          <SwiperSlide className={styles.swiper_slide}>
+            <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+          </SwiperSlide>
+          <SwiperSlide className={styles.swiper_slide}>
+            <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+          </SwiperSlide>
+          <SwiperSlide className={styles.swiper_slide}>
+            <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
