e13646ddfe07cd766aa1c7c38b9c9a9bb6457668
Adair <quesuabe@gmail.com>
2025-02-05 13:51:48 -0600

    forfun

--- src/pages/forfun/forfun.tsx
+++ src/pages/forfun/forfun.tsx
@@ -1,9 +1,20 @@
-import "./forfun.css";
+import style from "./forfun.module.css";
 
 function Forfun() {
   return (
     <div>
-      <p>ForFun</p>
+      <h1>ForFun</h1>
+      <div className={style.container}>
+        <div className={style.grid}>
+          <div className={style.item1}>1</div>
+
+          <div className={style.item2}>2</div>
+
+          <div className={style.item3}>3</div>
+
+          <div className={style.item4}>4</div>
+        </div>
+      </div>
     </div>
   );
 }
