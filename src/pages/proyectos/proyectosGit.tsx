9b34834c7c3fb1d22e01bf524d8e517d9d639a17
Adair <quesuabe@gmail.com>
2025-03-04 21:33:38 -0600

    Update proyectosGit.tsx

--- src/pages/proyectos/proyectosGit.tsx
+++ src/pages/proyectos/proyectosGit.tsx
@@ -14,7 +14,7 @@ function ProyetosGit() {
   useEffect(() => {
     fetch("https://api.github.com/users/AdairKevin/repos", {
       headers: {
-        Authorization: `hola`,
+        Authorization: ``,
       },
     })
       .then((response) => response.json())
